689005cc8807d0312ad8433d62243e0d8226ff2f
Bipin Baral <baburaja3421@gmail.com>
2024-06-10 14:26:07 +0545

    fixing issues

--- dist/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { login, register, getMyProfile, logout } from "../controllers/user.js";
-import { isAuthenticated } from "../middlewares/auth.js";
-const router = express.Router();
-router.post("/login", login);
-router.post("/new", register);
-router.get("/me", isAuthenticated, getMyProfile);
-router.get("/logout", logout);
-export default router;
--- dist/server.js
+++
@@ -1,8 +0,0 @@
-import { app } from "./app.js";
-import { connectDB } from "./data/database.js";
-import { config } from "dotenv";
-config({ path: "./.env" });
-connectDB();
-app.listen(process.env.port, () => {
-    console.log(`Server is working on port: ${process.env.port}`);
-});
